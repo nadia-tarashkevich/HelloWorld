1d45af4eb31406da3c078198529f484a963112cb
Nadia Tarashkevich <nadia.tarashkevich@jetbrains.com>
2022-08-30 13:52:38 -0700

    All

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Step1");
-        System.out.println("Step2");
-        System.out.println("Step3");
+        System.out.println("Step126");
+        System.out.println("Step4");
     }
 }
