833e3a0927c0d61343deae1d231f7ce5c4c3c887
Nadia Tarashkevich <nadia.tarashkevich@jetbrains.com>
2022-09-09 18:08:08 -0700

    All

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
+import java.io.IOException;
+
+
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Step1");
-        System.out.println("Step126");
-        System.out.println("Step4");
+    private static boolean createSparseFile(String filePath, Long fileSize) {
+        boolean success = true;
+        String command = "dd if=/dev/zero of=%s bs=1 count=1 seek=%s";
+        String formmatedCommand = String.format(command, filePath, fileSize);
+        String s;
+        Process p;
+        try {
+            p = Runtime.getRuntime().exec(formmatedCommand);
+
+            p.waitFor();
+            p.destroy();
+        } catch (IOException | InterruptedException e) {
+            return false;
+        }
+        return success;
     }
+    public static void main(String[] args) {
+        createSparseFile("long.txt", 2000000L);
+        var a = 10;
+    }
+
 }
